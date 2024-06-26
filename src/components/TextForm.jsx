0a70622f84fc72a5103c2674672229fe7aca211d
RimeshCdry <rimeshcdry45@gmail.com>
2024-06-26 17:34:40 +0545

    Added More Features

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react'
 
  function TextForm(props) {
-    
+
     const [text,setText] = useState("Write Something ...")
 
     const clickUpHandler = () =>{
@@ -21,18 +21,20 @@ import React, {useState} from 'react'
     }
 
   return (
-    <>
-        <div className="col-span-full py-6 w-screen bg-sky-400 h-screen">
+<>
+    <div className="col-span-full py-6 w-screen bg-sky-400 h-screen">
 
-        <h1 className='text-center text-3xl font-semibold mt-12 mb-4 text-white'>Write Some Text Here</h1>
+
+        <h1 className='text-center text-3xl font-semibold mt-5 mb-4 text-white'>Write Some Text Here</h1>
         <div className="mx-auto w-128">
+
                
 
             <textarea onChange={changeHandler}
-            id="mytext" name="mytext" rows="15" value={text}  className=" block container rounded-md border-2 outline-none p-3 mx-auto text-gray-900 shadow-lg placeholder:text-gray-400 ring-1 ring-inset ring-gray-300">
+            id="mytext" name="mytext" rows="8" value={text}  className=" block container rounded-md border-2 outline-none p-3 mx-auto text-gray-900 shadow-lg placeholder:text-gray-400 ring-1 ring-inset ring-gray-300">
 
             </textarea>
-                <div className='flex align-center gap-4'>
+                <div  className='flex align-center gap-4'>
                     <button onClick={clickUpHandler}
                     className='block px-4 py-2 bg-red-500 mt-3 rounded-md text-white font-medium  shadow-lg shadow-red-500/50'>
                         Convert to UpperCase
@@ -43,10 +45,26 @@ import React, {useState} from 'react'
                         Convert to LowerCase
                         </button>
                 </div> 
+
+                <div className='bg-white mt-4 p-5 rounded-lg outline-none shadow-xl'>
+                
+                <h2 className='text-3xl mb-2 text-blue-700 font-bold'>Text Summary</h2>
+                <p className='font-sans'>{text.split(" ").length} Words and {text.length} Characters</p>
+
+                <h2 className='text-2xl mb-1 mt-2 font-sans text-red-700 font-bold'>Text Preview </h2>
+                <p>{text}</p>
+
+                <h2 className='text-2xl mb-1 mt-9 font-sans text-cyan-400 font-bold'>Time to Read Text</h2>
+                <p>{0.008 * text.split(" ").length}</p>
+
         </div>
+        </div>
+
         
-        </div>
-    </>
+    </div>
+
+        
+</>
    
   )
 }
