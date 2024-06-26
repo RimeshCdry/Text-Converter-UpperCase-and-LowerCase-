1934ece174fc707ff003ac3b6b14b335714fcffa
RimeshCdry <rimeshcdry45@gmail.com>
2024-06-27 00:22:50 +0545

    Changing codes

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -22,16 +22,14 @@ import React, {useState} from 'react'
 
   return (
 <>
-    <div className="col-span-full py-6 w-screen bg-sky-400 h-screen">
-
-
-        <h1 className='text-center text-3xl font-semibold mt-5 mb-4 text-white'>Write Some Text Here</h1>
+    <div className="col-span-full py-6 w-screen bg-sky-400 h-screen overflow-y-scroll overflow-x-hidden" >
+        <div className='mx-auto'>
+            
+        <h1 className='text-center text-3xl font-semibold mt-2 mb-4 text-white'>Write Some Text Here</h1>
         <div className="mx-auto w-128">
 
-               
-
             <textarea onChange={changeHandler}
-            id="mytext" name="mytext" rows="8" value={text}  className=" block container rounded-md border-2 outline-none p-3 mx-auto text-gray-900 shadow-lg placeholder:text-gray-400 ring-1 ring-inset ring-gray-300">
+            id="mytext" name="mytext" rows="8" cols="20" value={text}  className=" block container rounded-md border-2 outline-none p-3 text-gray-900 shadow-lg placeholder:text-gray-400 ring-1 ring-inset ring-gray-300">
 
             </textarea>
                 <div  className='flex align-center gap-4'>
@@ -46,21 +44,20 @@ import React, {useState} from 'react'
                         </button>
                 </div> 
 
-                <div className='bg-white mt-4 p-5 rounded-lg outline-none shadow-xl'>
+                <div className='bg-white mt-4 p-3 rounded-lg outline-none shadow-xl'>
                 
-                <h2 className='text-3xl mb-2 text-blue-700 font-bold'>Text Summary</h2>
+                <h2 className='text-3xl mb-2 text-black font-bold'>Text Summary</h2>
                 <p className='font-sans'>{text.split(" ").length} Words and {text.length} Characters</p>
 
-                <h2 className='text-2xl mb-1 mt-2 font-sans text-red-700 font-bold'>Text Preview </h2>
+                <h2 className='text-2xl mb-1 mt-2 font-sans text-black font-bold'>Text Preview </h2>
                 <p>{text}</p>
 
-                <h2 className='text-2xl mb-1 mt-9 font-sans text-cyan-400 font-bold'>Time to Read Text</h2>
-                <p>{0.008 * text.split(" ").length}</p>
+                <h2 className='text-2xl mb-1 mt-9 font-sans text-black font-bold'>Time to Read Text</h2>
+                <p>{0.008 * text.split(" ").length} minutes</p>
 
+                </div>
         </div>
         </div>
-
-        
     </div>
 
         
